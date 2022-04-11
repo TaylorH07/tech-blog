8cbadf47a1566d9bda2ea151b0aa346cbc7f488f
Taylor Holmes <92659821+TaylorH07@users.noreply.github.com>
2022-04-11 18:47:00 -0500

    Update add-post.js

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -18,7 +18,7 @@ async function newFormHandler(event) {
     if (response.ok) {
         document.location.replace('/dashboard');
     } else {
-        alert(response.statusText);
+        alert('Please enter .com on post link form.');
     }
 }
   
